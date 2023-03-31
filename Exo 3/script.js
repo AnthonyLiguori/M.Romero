9ecb5182e85ccac43f45f341fb519bddabe66636
AnthonyLiguori <anthonat13@gmail.com>
2023-03-31 15:52:46 +0200

    Exo 4

--- Exo 3/script.js
+++ Exo 3/script.js
@@ -1,10 +1,11 @@
 function makeRequest() {
-  fetch("data.html")
-    .then(function (response) {
-      return response.text();
-    })
-
-    .then(function (data) {
-      alert(data);
-    });
-}
+    fetch("data.html")
+      .then(function (response) {
+        return response.text();
+      })
+  
+      .then(function (data) {
+       document.write(data)
+      });
+  }
+  
